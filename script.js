b76755d33786f33fe7f561310223ca9c449d5a9f
Kirk Mendoza <kirkmendoza@gmail.com>
2025-04-13 08:25:25 -0500

    addNewPlayer - code clean up

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const API_URL = "https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-et-web-am-PUP
 let puppies = []
 const puppiesListDiv = document.querySelector("#puppiesList")
 const addPuppyForm = document.querySelector("#addPuppyForm")
+console.log( addPuppyForm.name.value )
 
 window.addEventListener("hashchange", (event) => {
   render()
@@ -18,11 +19,11 @@ window.addEventListener("hashchange", (event) => {
 const fetchAllPlayers = async () => {
   //TODO
   try {
-    const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-et-web-am-PUPPIES/players")
+    const response = await fetch(API_URL)
     const data = await response.json()
-    console.log(data)
     puppies = data.data.players
-    console.log(puppies)
+    // console.log(data)
+    // console.log(puppies)
     render()
   } catch (error) {
     console.error(error)
@@ -30,7 +31,6 @@ const fetchAllPlayers = async () => {
 
 };
 
-fetchAllPlayers()
 
 
 
@@ -60,7 +60,7 @@ const addNewPlayer = async (newPlayer) => {
   console.log("newPuppy", newPuppy)
 
   try {
-    const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-et-web-am-PUPPIES/players`, {
+    const response = await fetch(API_URL , {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -68,9 +68,18 @@ const addNewPlayer = async (newPlayer) => {
       body: JSON.stringify(newPuppy)
     })
     const data = await response.json()
-    console.log("data", data)
     puppies.push(data.data.newPlayer)
-    console.log("new puppies:", puppies)
+    // console.log("data", data)
+    // console.log("new puppies:", puppies)
+
+    // Clear form
+    addPuppyForm.name.value = ""
+    addPuppyForm.breed.value = ""
+    addPuppyForm.status.value = ""
+    addPuppyForm.imageUrl.value = ""
+    addPuppyForm.teamId.value = ""
+
+    // Render
     render()
   } catch (error) {
     console.error(error)
@@ -78,38 +87,6 @@ const addNewPlayer = async (newPlayer) => {
 };
 
 addPuppyForm.addEventListener("submit", addNewPlayer)
-
-// addPuppyForm.addEventListener("submit", async (newPlayer) => {
-//   console.log("Line 83")
-//   console.log("newPlayer", newPlayer)
-//   newPlayer.preventDefault()
-
-//   const newPuppy = {
-//     name: newPlayer.target.name.value,
-//     breed: newPlayer.target.breed.value,
-//     status: newPlayer.target.status.value,
-//     imageUrl: newPlayer.target.imageUrl.value,
-//     teamId: newPlayer.target.imageUrl.value*1,
-//   }
-//   console.log("newPuppy", newPuppy)
-
-//   try {
-//     const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-et-web-am-PUPPIES/players`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(newPuppy)
-//     })
-//     const data = await response.json()
-//     console.log("data", data)
-//     puppies.push(data.data.newPlayer)
-//     console.log("new puppies:", puppies)
-//     render()
-//   } catch (error) {
-//     console.error(error)
-//   }
-// })
 
 
 
@@ -130,17 +107,23 @@ const render = () => {
     return `
       <a href=#${puppy.name}>
         <div class="puppyCard">
+          <img src="${puppy.imageUrl}" />
           <h3>${puppy.name}</h3>
         </div>
       </a>
     `
-    // <img src="${puppy.imageUrl}" />
   })
  
   const name = window.location.hash.slice(1)
   console.log(name)
   
   puppiesListDiv.innerHTML = html.join("")
+
+  const singlePlayer = puppies.find((player) => {
+    return player.name === name
+  })
+
+  // console.log(singlePlayer)
 
 };
 
@@ -159,11 +142,11 @@ const renderSinglePlayer = (player) => {
 // Initializes the app by calling render
 const init = async () => {
   //Before we render, what do we always need...?
-
-  render();
-
+  fetchAllPlayers()
+  // render();
 };
 
+init()
 
 
 
